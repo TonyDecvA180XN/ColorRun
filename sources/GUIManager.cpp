8e47bbe04cc73a75dfc3490f7e9e23c8900a2847
TonyDecvA180XN <tony.dospiekhov@gmail.com>
2021-06-16 15:25:09 +0300

    Implement complete mixing widget. Add background panel. Add display for current active color. Apply active color to character.

--- sources/GUIManager.cpp
+++ sources/GUIManager.cpp
@@ -1,41 +1,94 @@
 #include "GUIManager.h"
 #include "MathUtils.h"
+#define b2f(b) (b ? 1.0f : 0.0f)
+
+using namespace std::string_literals;
 
 void GUIManager::BuildUI(w4::sptr<w4::gui::Widget> root, std::string path)
 {
-	static constexpr SIZE cWidth = 1080, cHeight = 1920;
+    w4::gui::setVirtualResolution({ cWidth, cHeight });
+    
+    // create current color display
+    auto colorLabel = w4::gui::createWidget<w4::gui::Label>(root, "Current Color:       ");
+    colorLabel->setPosition({ static_cast<INT>(cWidth / 2), static_cast<INT>(cHeight / 8) });
+    colorLabel->setSizePolicy(w4::gui::SizePolicy::Fixed, w4::gui::SizePolicy::Fixed);
+    colorLabel->setHorizontalAlign(w4::gui::HorizontalAlign::Center);
+    colorLabel->setVerticalAlign(w4::gui::VerticalAlign::Center);
+    colorLabel->setSize({600, 120});
+    colorLabel->setFont("https://fonts.googleapis.com/css2?family=Roboto", "Roboto");
+    colorLabel->setFontSize(64);
+    colorLabel->setTextAlign(w4::gui::HorizontalAlign::Left, w4::gui::VerticalAlign::Center);
 
-	w4::gui::setVirtualResolution({ cWidth, cHeight });
-	
-	// create current color display
-	std::string filename = "000";
-	m_currentColor = w4::gui::createWidget<w4::gui::ImageButton>(root, path + filename + ".png", path + filename + ".png", w4::math::ivec2{ 100, 100 });
-	m_currentColor->setHorizontalAlign(w4::gui::HorizontalAlign::Center);
-	m_currentColor->setVerticalAlign(w4::gui::VerticalAlign::Center);
-	m_currentColor->setSizePolicy(w4::gui::SizePolicy::Fixed, w4::gui::SizePolicy::Fixed);
-	m_currentColor->setPosition({ static_cast<INT>(cWidth / 2), static_cast<INT>(cHeight / 4) });
-	
-	// create color circle
-	for (INDEX i = 0; i != 3; ++i)
-	{
-		std::string filename = "000";
-		filename[i] = '1';
-		filename = path + filename + ".png";
-		m_selectors[i] = w4::gui::createWidget<w4::gui::ImageButton>(root, filename, filename, w4::math::ivec2{100, 100});
-		m_selectors[i]->setHorizontalAlign(w4::gui::HorizontalAlign::Center);
-		m_selectors[i]->setVerticalAlign(w4::gui::VerticalAlign::Center);
-		m_selectors[i]->setSizePolicy(w4::gui::SizePolicy::Fixed, w4::gui::SizePolicy::Fixed);
-		
-		FLOAT angle = 90_deg + (i * 120_deg);
-		FLOAT radius = 100;
-		m_selectors[i]->setPosition(
-			{ 
-			static_cast<INT>(radius * std::cosf(angle) + cWidth / 2),
-			static_cast<INT>(radius * std::sinf(angle) + cHeight * 3 / 4)
-			});
-		m_selectors[i]->onTap([this, filename]()
-			{
-				this->m_currentColor->setImage(filename);
-			});
-	}
+    m_currentColor = w4::gui::createWidget<w4::gui::Image>(root, path + "000.png", w4::math::ivec2{ 100, 100 });
+    m_currentColor->setHorizontalAlign(w4::gui::HorizontalAlign::Center);
+    m_currentColor->setVerticalAlign(w4::gui::VerticalAlign::Center);
+    m_currentColor->setSizePolicy(w4::gui::SizePolicy::Fixed, w4::gui::SizePolicy::Fixed);
+    m_currentColor->setPosition({ static_cast<INT>(cWidth * 45 / 64), static_cast<INT>(cHeight / 8) });
+
+    // panel
+    auto colorPanel = w4::gui::createWidget<w4::gui::Image>(root, path + "panel.png", w4::math::ivec2{ 400, 400 });
+    colorPanel->setHorizontalAlign(w4::gui::HorizontalAlign::Center);
+    colorPanel->setVerticalAlign(w4::gui::VerticalAlign::Center);
+    colorPanel->setSizePolicy(w4::gui::SizePolicy::Fixed, w4::gui::SizePolicy::Fixed);
+    colorPanel->setPosition({ static_cast<INT>(cWidth / 2), static_cast<INT>(cHeight * 3 / 4) });
+    
+    // create color circle
+    for (INDEX i = 0; i != 3; ++i)
+    {
+        std::string filename = "000";
+        filename[i] = '1';
+        filename = path + filename + ".png";
+        m_selectors[i] = w4::gui::createWidget<w4::gui::Image>(root, filename, w4::math::ivec2{100, 100});
+        m_selectors[i]->setHorizontalAlign(w4::gui::HorizontalAlign::Center);
+        m_selectors[i]->setVerticalAlign(w4::gui::VerticalAlign::Center);
+        m_selectors[i]->setSizePolicy(w4::gui::SizePolicy::Fixed, w4::gui::SizePolicy::Fixed);
+        
+        FLOAT angle = 90_deg + (i * 120_deg);
+        FLOAT radius = 80;
+        m_selectorPositions[i] = {
+            static_cast<INT>(radius * std::cosf(angle) + cWidth / 2),
+            static_cast<INT>(radius * std::sinf(angle) + cHeight * 3 / 4)
+        };
+
+        m_selectors[i]->setPosition(m_selectorPositions[i]);
+    }
+    
+    m_currentColor->setImage("ui/111.png");
+    m_combo.reset();
+}
+
+w4::math::vec4 GUIManager::Update(w4::math::vec2 cursor)
+{
+    for (INDEX i = 0; i != 3; ++i)
+    {
+        w4::math::ivec2 virtualCursor{ 
+            static_cast<INT>(cursor.w * cWidth),
+            static_cast<INT>(cursor.h * cHeight)
+        };
+        if (std::hypotf(m_selectorPositions[i].x - virtualCursor.x, m_selectorPositions[i].y - virtualCursor.y) <= 50)
+        {
+            // bit ordering is reversed
+            if (!m_combo.test(2 - i))
+            {
+                m_combo.set(2 - i);
+                if (m_combo.all())
+                {
+                    m_currentColor->setImage("ui/000.png");
+                    return { 0, 0, 0, 1 };
+                }
+
+                else
+                {
+                    m_currentColor->setImage("ui/" + m_combo.to_string() + ".png");
+                    return { b2f(m_combo[2]), b2f(m_combo[1]), b2f(m_combo[0]), 1 };
+                }
+            }
+        }
+    }
+    return { 0, 0, 0, 0 };
+}
+
+void GUIManager::Reset()
+{
+    m_combo.reset();
 }
--- sources/GUIManager.h
+++ sources/GUIManager.h
@@ -6,9 +6,18 @@
 class GUIManager
 {
 public:
+	static constexpr SIZE cWidth = 1080, cHeight = 1920;
+
 	void BuildUI(w4::sptr<w4::gui::Widget> root, std::string path);
 
+	w4::math::vec4 Update(w4::math::vec2 cursor);
+
+	void Reset();
+
 private:
-	w4::sptr<w4::gui::ImageButton> m_selectors[3];
-	w4::sptr<w4::gui::ImageButton> m_currentColor;
+	w4::sptr<w4::gui::Image> m_selectors[3];
+	w4::sptr<w4::gui::Image> m_currentColor;
+
+	w4::math::ivec2 m_selectorPositions[3];
+	std::bitset<3> m_combo;
 };
